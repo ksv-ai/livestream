cbddaffd247ae487d89489cde20fad93d31d3bfa
ksv-ai <keshav.laptop1@gmail.com>
2026-07-06 16:40:28 +0545

    created kptv folder

---
+++ kptv/dataa.js
@@ -0,0 +1,191 @@
+window.KP_DATA = {
+    "fifa2": {
+        "match": {
+            "title": "Mexico vs England",
+            "date": "07 July 2026",
+            "time": "02:45 AM NST",
+            "stadium": "NRG Stadium",
+            "league": "FIFA World Cup",
+            "round": "Round of 32",
+            "broadcaster": "beIN Sports,Sky Go UK,NBC",
+            "banner_image": "https://theanalyst.com/wp-content/uploads/2026/07/England-v-Mexico-preview.jpg",
+            "lineup_published": true
+        },
+        "team1": {
+            "name": "Mexico",
+            "lineup": "Simon; Llorente, Laporte, Cubarsi, Cucurella; Rodri, Ruiz, Gavi, Pedri; Torres, Oyarzabal"
+        },
+        "team2": {
+            "name": "England",
+            "lineup": "Vozinha; Borges, Lopes, Cabral, Moreira; Pina, Monteiro, Duarte; Cabral, Livramento, Mendes"
+        },
+        "telecast_links": [
+            {
+                "name": "ALL Link 2:Mexico vs England",
+                "url": "https://yonotv-now.pages.dev/page.html?src=https://yonotv-now.pages.dev/LINK2?id=Mexico"
+            },
+            {
+                "name": "HD Link 2:Mexico vs England",
+                "url": "https://yonotv-now.pages.dev/page.html?src=https://yonotv-now.pages.dev/LINK2?id=Mexico2"
+            },
+            {
+                "name": "HD Link 3:Mexico vs England",
+                "url": "https://yonotv-now.pages.dev/page.html?src=https://yonotv-now.pages.dev/LINK2?id=Mexico3"
+            },
+            {
+                "name": "HD Link 4:Mexico vs England",
+                "url": "https://yonotv-now.pages.dev/page.html?src=https://yonotv-now.pages.dev/LINK2?id=Mexico4"
+            },
+            {
+                "name": "HD 6:Mexico vs England",
+                "url": "https://yonotv-now.pages.dev/page.html?src=https://yonotv-now.pages.dev/LINK2?id=Mexico5"
+            },
+            {
+                "name": "Link 8:Mexico vs England",
+                "url": "https://yonotv-now.pages.dev/page.html?src=https://yonotv-now.pages.dev/LINK2?id=Mexico6"
+            },
+            {
+                "name": "Link 9:Mexico vs England",
+                "url": "https://yonotv-now.pages.dev/page.html?src=https://yonotv-now.pages.dev/LINK2?id=Mexico7"
+            },
+            {
+                "name": "Link 10:Mexico vs England",
+                "url": "https://yonotv-now.pages.dev/page.html?src=https://yonotv-now.pages.dev/LINK2?id=Mexico8"
+            },
+            {
+                "name": "Link 12:Mexico vs England",
+                "url": "https://yonotv-now.pages.dev/page.html?src=https://yonotv-now.pages.dev/LINK2?id=Mexico9"
+            },
+            {
+                "name": "Link 13:Mexico vs England",
+                "url": "https://yonotv-now.pages.dev/page.html?src=https://yonotv-now.pages.dev/LINK2?id=Mexico10"
+            },
+            {
+                "name": "Link 14:Mexico vs England",
+                "url": "https://yonotv-now.pages.dev/page.html?src=https://yonotv-now.pages.dev/LINK2?id=Mexico11"
+            }
+        ]
+    },
+    "fifa3": {
+        "match": {
+            "title": "Colombia vs Ghana",
+            "date": "04 July 2026",
+            "time": "07:15 AM NST",
+            "stadium": "NRG Stadium",
+            "league": "FIFA World Cup",
+            "round": "Round of 32",
+            "broadcaster": "beIN Sports,Sky Go UK,NBC",
+            "banner_image": "https://theanalyst.com/wp-content/uploads/2026/07/colombia-vs-ghana-match-preview-opta.jpg",
+            "lineup_published": true
+        },
+        "team1": {
+            "name": "Colombia",
+            "lineup": "Simon; Llorente, Laporte, Cubarsi, Cucurella; Rodri, Ruiz, Gavi, Pedri; Torres, Oyarzabal"
+        },
+        "team2": {
+            "name": "Ghana",
+            "lineup": "Vozinha; Borges, Lopes, Cabral, Moreira; Pina, Monteiro, Duarte; Cabral, Livramento, Mendes"
+        },
+        "telecast_links": [
+            {
+                "name": "ALL Link 2:Colombia vs Ghana",
+                "url": "https://yonotv-now.pages.dev/page.html?src=https://yonotv-now.pages.dev/LINK2?id=Colombia"
+            },
+            {
+                "name": "HD Link 1:Colombia vs Ghana",
+                "url": "https://yonotv-now.pages.dev/page.html?src=https://yonotv-now.pages.dev/LINK2?id=Colombia2"
+            },
+            {
+                "name": "HD Link 3:Colombia vs Ghana",
+                "url": "https://yonotv-now.pages.dev/page.html?src=https://yonotv-now.pages.dev/LINK2?id=Colombia3"
+            },
+            {
+                "name": "HD Link 4:Colombia vs Ghana",
+                "url": "https://yonotv-now.pages.dev/page.html?src=https://yonotv-now.pages.dev/LINK2?id=Colombia4"
+            },
+            {
+                "name": "Full HD 6:Colombia vs Ghana",
+                "url": "https://yonotv-now.pages.dev/page.html?src=https://yonotv-now.pages.dev/LINK2?id=Colombia5"
+            },
+            {
+                "name": "Link 8:Colombia vs Ghana",
+                "url": "https://yonotv-now.pages.dev/page.html?src=https://yonotv-now.pages.dev/LINK2?id=Colombia6"
+            },
+            {
+                "name": "Link 9:Colombia vs Ghana",
+                "url": "https://yonotv-now.pages.dev/page.html?src=https://yonotv-now.pages.dev/LINK2?id=Colombia7"
+            },
+            {
+                "name": "Link 10:Colombia vs Ghana",
+                "url": "https://yonotv-now.pages.dev/page.html?src=https://yonotv-now.pages.dev/LINK2?id=Colombia8"
+            },
+            {
+                "name": "Link 12:Colombia vs Ghana",
+                "url": "https://yonotv-now.pages.dev/page.html?src=https://yonotv-now.pages.dev/LINK2?id=Colombia9"
+            },
+            {
+                "name": "Link 13:Colombia vs Ghana",
+                "url": "https://yonotv-now.pages.dev/page.html?src=https://yonotv-now.pages.dev/LINK2?id=Colombia10"
+            },
+            {
+                "name": "Link 14:Colombia vs Ghana",
+                "url": "https://yonotv-now.pages.dev/page.html?src=https://yonotv-now.pages.dev/LINK2?id=Colombia11"
+            }
+        ]
+    },
+    "fifa4": {
+        "match": {
+            "title": "FIFA World Cup",
+            "date": "22 Jun 2026",
+            "time": "12:45 AM NST",
+            "stadium": "Camp Nou Stadium",
+            "league": "FIFA World Cup",
+            "round": "Group Stage",
+            "broadcaster": "beIN Sports,Sky Go UK,NBC",
+            "banner_image": "https://ichef.bbci.co.uk/images/ic/raw/p0nr8w9y.jpg",
+            "lineup_published": false
+        },
+        "team1": {
+            "name": "FIFA",
+            "lineup": "XX"
+        },
+        "team2": {
+            "name": "Ghana",
+            "lineup": "XX"
+        },
+        "telecast_links": [
+            {
+                "name": "ALL Link 2:FIFA World Cup",
+                "url": "https://yonotv-now.pages.dev/page.html?src=https://yonotv-now.pages.dev/LINK2?id=FIFA"
+            },
+            {
+                "name": "HD Link 2:FIFA World Cup",
+                "url": "https://yonotv-now.pages.dev/page.html?src=https://yonotv-now.pages.dev/LINK2?id=FIFA2"
+            },
+            {
+                "name": "HD Link 3:FIFA World Cup",
+                "url": "https://yonotv-now.pages.dev/page.html?src=https://yonotv-now.pages.dev/LINK2?id=FIFA3"
+            },
+            {
+                "name": "HD Link 4:FIFA World Cup",
+                "url": "https://yonotv-now.pages.dev/page.html?src=https://yonotv-now.pages.dev/LINK2?id=FIFA4"
+            },
+            {
+                "name": "IOS Link 6:FIFA World Cup",
+                "url": "https://yonotv-now.pages.dev/page.html?src=https://yonotv-now.pages.dev/LINK2?id=FIFA5"
+            },
+            {
+                "name": "PC Link 7:FIFA World Cup",
+                "url": "https://yonotv-now.pages.dev/page.html?src=https://yonotv-now.pages.dev/LINK2?id=FIFA6"
+            },
+            {
+                "name": "PC Link 8:FIFA World Cup",
+                "url": "https://yonotv-now.pages.dev/page.html?src=https://yonotv-now.pages.dev/LINK2?id=FIFA7"
+            },
+            {
+                "name": "PC Link 9:FIFA World Cup",
+                "url": "https://yonotv-now.pages.dev/page.html?src=https://yonotv-now.pages.dev/LINK2?id=FIFA8"
+            }
+        ]
+    }
+};
